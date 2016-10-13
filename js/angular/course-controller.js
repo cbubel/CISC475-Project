0033d2443903df9f7718518411bb9f07a8e2da5a
Natalie Lane <nlane@udel.edu>
2016-10-13 15:58:30 -0400

    Course Control Starter
    Added outline of course controller and corresponding function in fb
    service

---
+++ js/angular/course-controller.js
@@ -0,0 +1,16 @@
+(function () {
+    var app = angular.module('baseApp');
+
+    app.controller('courseCtrl', ['$scope', 'firebaseService', function($scope, firebaseService) {
+        $scope.courses = [];
+
+        $scope.getCourses = function() {
+            firebaseService.getCourses().then(function(courses) {
+                $scope.courses = courses;
+                $scope.$apply();
+            });
+        };
+
+        $scope.getCourses();
+    }]);
+})();
--- js/angular/firebase-service.js
+++ js/angular/firebase-service.js
@@ -1,9 +1,9 @@
 (function () {
     var app = angular.module('baseApp');
-  
+
     app.service("firebaseService", function() {
         var db = firebase.database();
-        
+
         this.getStudents = function() {
             return db.ref("students").once("value").then(function(snapshot) {
                 return snapshot.val();
@@ -12,6 +12,14 @@
 
         this.addStudent = function(student) {
             db.ref("students").push(student);
+        };
+
+        // I don't have access to db right now, but I am leaving the field
+        // labelled as "courses"
+        this.getCourses = function(){
+          return db.ref("courses").once("value").then(function(snapshot) {
+            return snapshot.val();
+          });
         };
 
     });
