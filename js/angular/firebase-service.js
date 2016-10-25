8cd8e3f6006bf8801100d8c9d2ed824137ebfabf
Natalie Lane <nlane@udel.edu>
2016-10-24 23:31:49 -0400

    Firebase Functions
    Added Gets, started Update

--- js/angular/firebase-service.js
+++ js/angular/firebase-service.js
@@ -13,16 +13,50 @@ app.service("firebaseService", function() {
         db.ref("students").push(student);
     };
 
+
+    this.getStudentById = function(id){
+      return db.ref("students").orderByChild("id").equalTo(id).on("child_added", function(snapshot) {
+        return snapshot.val();
+      }, function(error) {
+        console.error(error);
+      });
+    };
+
+    // Does not work yet, is inserting as a sub-item
+    this.updateStudent = function(student){
+      db.ref("students").orderByChild("id").equalTo(student.id).on('value', function(snapshot) {
+        console.log(snapshot.val());
+        console.log(student);
+        if(snapshot != null){
+          db.ref("students/" + snapshot.key).update(student);
+        }
+        else{
+          console.error("Couldn't find student");
+        }
+      }, function(error) {
+        // The Promise was rejected.
+        console.error(error);
+    });
+    }
+
+
+    this.getCourseById = function(id){
+      return db.ref("courses").orderByChild("courseID").equalTo(id).on("child_added", function(snapshot) {
+         console.log(snapshot.key);
+         console.log(snapshot.val());
+        return snapshot.val();
+      }, function(error){
+        console.error(error);
+      });
+    };
+
     this.addCourse = function(course) {
         db.ref("courses").push(course);
     };
 
-    // I don't have access to db right now, but I am leaving the field
-    // labelled as "courses"
     this.getCourses = function(){
         return db.ref("courses").once("value").then(function(snapshot) {
             return snapshot.val();
         });
     };
-
 });
