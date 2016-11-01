07ca7e74bb66870928077987c515c5c3b06a8bf2
Chris Bubel <christopher.s.bubel@gmail.com>
2016-10-31 22:04:34 -0400

    Updated view students page, now can click to edit

--- js/angular/controllers/students-controller.js
+++ js/angular/controllers/students-controller.js
@@ -1,22 +1,14 @@
 var app = angular.module('baseApp');
 
 app.controller('studentsCtrl', ['$scope', 'firebaseService', function($scope, firebaseService) {
-  $scope.students = [];
+  $scope.students = {};
 
-  var error = function(err) {
-    console.log("called error");
-    console.log(err);
-  }
+  firebaseService.getStudents(function(students) {
+    $scope.students = students;
+    $scope.$apply();
+    console.log($scope.students);
+  }, function(error) {
+    console.log(error);
+  });
 
-  $scope.getStudents = function() {
-    firebaseService.getStudents().then(function(students) {
-      $scope.students = students;
-      $scope.$apply();
-    });
-  };
-
-  var something = firebaseService.updateStudent("-KUcJpOM8ztJ2R0YNU", {hello: "world"}, function(result) {
-    console.log(result);
-  }, error);
-  $scope.getStudents();
 }]);
