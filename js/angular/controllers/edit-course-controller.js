7d7e31de46bbe82458e3afab09d250e274c73291
mattrspicer <mspicer@udel.edu>
2016-11-01 13:00:09 -0400

    added required fields logic to edit course controller

--- js/angular/controllers/edit-course-controller.js
+++ js/angular/controllers/edit-course-controller.js
@@ -11,17 +11,47 @@ app.controller('editCourseCtrl', ['$scope', '$location', '$routeParams', 'fireba
     $scope.course.removeSection(idx);
   };
 
+  function areReqFieldsFilled() {
+    return areReqCourseFieldsFilled() && areReqSectionFieldsFilled();
+  }
+
+  function areReqCourseFieldsFilled() {
+    if ($scope.course.courseID === "") {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+
+  function areReqSectionFieldsFilled() {
+    for (var i = 0; i < $scope.course.sections.length; i++) {
+      var section = $scope.course.sections[i];
+      if (section.sectionID === "" || section.instructor === "" || section.startTime === "" || section.endTime === "" ||
+          section.undergradTAsNeeded === 0 || section.gradTAsNeeded === 0 || section.undergradLAsNeeded === 0) {
+            return false;
+      }
+    }
+    return true;
+  }
+
   $scope.submit = function() {
-      $scope.course.sections.forEach(function(section) {
-          delete section["$$hashKey"];
-      });
-      firebaseService.addCourse($scope.course, function(result) {
-        toastr.success("Added Course");
-        console.log(result);
-      }, function(error) {
-        toastr.error("Failed to add");
-        console.log(error);
-      });
+      if (areReqFieldsFilled()) {
+        $scope.course.sections.forEach(function(section) {
+            delete section["$$hashKey"];
+        });
+        firebaseService.addCourse($scope.course, function(result) {
+          toastr.success("Updated Course");
+          console.log(result);
+        }, function(error) {
+          toastr.error("Failed to update");
+          console.log(error);
+        });
+      }
+      else {
+        toastr.error("Required Fields Not Filled");
+      }
+
   };
 
 }]);
