acdc5b0675af141e3c3f792165a9ee3985cbabab
Chris Bubel <christopher.s.bubel@gmail.com>
2016-11-07 11:22:43 -0500

    Added very basic prettify days function that will likely need to be altered depending on ordering - probably need rules first

--- js/angular/controllers/courses-controller.js
+++ js/angular/controllers/courses-controller.js
@@ -3,6 +3,14 @@ var app = angular.module('baseApp');
 app.controller('coursesCtrl', ['$scope', 'firebaseService', function($scope, firebaseService) {
   $scope.courses = {};
 
+  $scope.prettifyDays = function(days) {
+    var res = "";
+    for(var day in days) {
+      res += day.slice(0, 1).toUpperCase();
+    }
+    return res;
+  }
+
   firebaseService.getCourses(function(courses) {
     $scope.courses = courses;
     console.log(courses);
