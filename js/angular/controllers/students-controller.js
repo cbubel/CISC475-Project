e283fe2d494f84c4db20ae5fa007e52404cf3723
Mooshad <jakep@udel.edu>
2016-11-14 22:41:51 -0500

    Progress on expanding cells in students form.

--- js/angular/controllers/students-controller.js
+++ js/angular/controllers/students-controller.js
@@ -10,5 +10,48 @@ app.controller('studentsCtrl', ['$scope', 'firebaseService', function($scope, fi
   }, function(error) {
     console.log(error);
   });
+  
+  
+  // The following code is used for expanding the tables in students.html
+  
+  $scope.oneAtATime = true;
+  $scope.tableRowExpanded = false;
+  $scope.tableRowIndexExpandedCurr = "";
+  $scope.storeIdExpanded = "";
+
+  $scope.dataCollapseFn = function(){
+	  $scope.DataCollapse = [];
+	  for (var i = 0; i < $scope.students.length; i+=1){
+		  $scope.DataCollapse.push(false);
+	  }
+  };
+  
+  $scope.selectTableRow = function(index, storeId) {
+	  if(typeof $scope.DataCollapse === 'undefined'){
+		  $scope.dataCollapseFn();
+	  }
+	  
+	  if ($scope.tableRowExpanded === false && $scope.tableRowIndexExpandedCurr === "" && $scope.storeIdExpanded === "") {
+            $scope.tableRowIndexExpandedPrev = "";
+            $scope.tableRowExpanded = true;
+            $scope.tableRowIndexExpandedCurr = index;
+            $scope.storeIdExpanded = storeId;
+            $scope.DataCollapse[index] = true;
+        } else if ($scope.tableRowExpanded === true) {
+            if ($scope.tableRowIndexExpandedCurr === index && $scope.storeIdExpanded === storeId) {
+                $scope.tableRowExpanded = false;
+                $scope.tableRowIndexExpandedCurr = "";
+                $scope.storeIdExpanded = "";
+                $scope.DataCollapse[index] = false;
+            } else {
+                $scope.tableRowIndexExpandedPrev = $scope.tableRowIndexExpandedCurr;
+                $scope.tableRowIndexExpandedCurr = index;
+                $scope.storeIdExpanded = storeId;
+                $scope.DataCollapse[$scope.tableRowIndexExpandedPrev] = false;
+                $scope.DataCollapse[$scope.tableRowIndexExpandedCurr] = true;
+            }
+        }
+
+    };
 
 }]);
