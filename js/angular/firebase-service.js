f0d3e3852bdb4f3292cd2904d1fd0239c8c13608
Natalie Lane <nlane@udel.edu>
2016-11-21 16:36:32 -0500

    Added Firebase functions for assignments

--- js/angular/firebase-service.js
+++ js/angular/firebase-service.js
@@ -120,4 +120,86 @@ app.service("firebaseService", function() {
     });
   };
 
+  // adds the given student to the course's candidate choices
+  // takes in the firebase id of the student and course
+  this.addCandidateAssignment = function(studentID, courseID, success, failure) {
+    db.ref(`assignments/${courseID}/candidates`).push(studentID)
+    .then(function(snapshot) {
+      success("Successfully added student");
+    }, function(error) {
+      failure(error);
+    });
+  };
+
+  // adds the given student to the course's final choices
+  // takes in the firebase id of the student and course
+  this.addFinalAssignment = function(studentID, courseID, success, failure) {
+    db.ref(`assignments/${courseID}/final`).push(studentID)
+    .then(function(snapshot) {
+      success("Successfully added student");
+    }, function(error) {
+      failure(error);
+    });
+  };
+
+  // returns all the studenIDs of the candidate choices for the given firebase courseID
+  this.getCandidates = function(courseID, success, failure) {
+    return db.ref(`assignments/${courseID}/candidates`).once("value")
+    .then(function(snapshot) {
+      success(snapshot.val());
+    }, function(error) {
+      failure(error);
+    });
+  };
+
+  // returns all the studentIDs of the final choices for the given firebase courseID
+  this.getFinalChoice = function(courseID, success, failure) {
+    return db.ref(`assignments/${courseID}/final`).once("value")
+    .then(function(snapshot) {
+      success(snapshot.val());
+    }, function(error) {
+      failure(error);
+    });
+  };
+
+  // Use this to get a json of all assignments. Each course is mapped to a candidates
+  // key and a final key. The values of these keys are firebase ids of students.
+  // assignments -> courseID -> candidates/final -> studentID
+  this.getAllAssignments = function(success, failure) {
+    return db.ref(`assignments`).once("value")
+    .then(function(snapshot) {
+      success(snapshot.val());
+    }, function(error) {
+      failure(error);
+    });
+  };
+
+  // Will remove student from course's candidates. Must take in the firebase ID
+  // of the assignment (not the student's ID), and the firebase ID of the course
+  this.removeCandidate = function(firebaseID, courseID, success, failure) {
+    db.ref(`assignments/${courseID}/candidates/${firebaseID}`).remove()
+    .then(function(snapshot) {
+      success("Successfully added student");
+    }, function(error) {
+      failure(error);
+    });
+  };
+
+  // Will remove student from course's final field. Must take in the firebase ID
+  // of the assignment (not the student's ID), and the firebase ID of the course
+  this.removeCandidate = function(firebaseID, courseID, success, failure) {
+    db.ref(`assignments/${courseID}/final/${firebaseID}`).remove()
+    .then(function(snapshot) {
+      success("Successfully added student");
+    }, function(error) {
+      failure(error);
+    });
+  };
+
+//  this.removeCandidate("-KX7iJZTgAZVVYKEFbOi", "-KW04wfI9hVWP_2m38ap", "success", "failure");
+//  this.getFinalChoice("-KW04wfI9hVWP_2m38ap", "success", "failure");
+//  this.getCandidates("-KW04wfI9hVWP_2m38ap", "success", "failure");
+//  this.getAllAssignments("success", "failure");
+//this.addCandidateAssignment("-KW05_j8ec1kR90BxdJi", "-KW04wfI9hVWP_2m38ap", "success", "failure");
+//this.addFinalAssignment("-KW04mhLzhP__N6JRomT", "-KW04wfI9hVWP_2m38ap", "success", "failure");
 });
