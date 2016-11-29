adef297f841308f0d98d0858b94ac638e1983aee
mattrspicer <mspicer@udel.edu>
2016-11-29 18:29:49 -0500

    Changes to Assign Candidates

--- js/angular/controllers/candidates-controller.js
+++ js/angular/controllers/candidates-controller.js
@@ -6,39 +6,36 @@ app.controller('candidatesCtrl', ['$scope', 'firebaseService', function($scope, 
 
         $scope.courseAssignments = [];
 
-        courses.forEach(function(course) {
+        courses.forEach(function(course, index) {
 
             // get the candidates for this course
             firebaseService.getCandidates(course.firebaseId, function(result) {
 
                 // get key candidate info for this course
                 var candidates = [];
-                (function() {
-                  result.forEach(function(candidate) {
-                      firebaseService.getStudentById(candidate.studentId, function(student) {
-                          var candidateInfo = {};
-                          candidateInfo.name = student.first_name + " " + student.last_name;
-                          candidateInfo.section = candidate.section;
-                          candidateInfo.studentID = student.id;
-                          candidates.push(candidateInfo);
 
-                          // once we have all the info about candidates we need, we push the candidates list
-                          if (candidates.length == result.length) {
-                              $scope.courseAssignments.push([course, candidates]);
-                              $scope.$apply();
-                          };
+                $scope.courseAssignments.push([course, candidates]);
+                $scope.$apply()
 
-                      }, function(error) {
-                          console.log(error);
-                      });
-                  });
+                result.forEach(function(candidate) {
+                    firebaseService.getStudentById(candidate.studentId, function(student) {
+                        var candidateInfo = {};
+                        candidateInfo.name = student.first_name + " " + student.last_name;
+                        candidateInfo.section = candidate.section;
+                        candidateInfo.studentID = student.id;
+                        candidates.push(candidateInfo);
 
-                  if (result.length == 0 || typeof result == 'undefined') {
-                      $scope.courseAssignments.push([course, candidates]);
-                      $scope.$apply();
-                  };
+                        // once we have all the info about candidates we need, we push the candidates list
+                        if (candidates.length == result.length) {
+                            $scope.courseAssignments[index] = [course, candidates];
+                            $scope.$apply();
+                        };
 
-                })();
+                    }, function(error) {
+                        console.log(error);
+                    });
+                });
+
             }, function(error) {
                 console.log(error);
             });
@@ -48,5 +45,7 @@ app.controller('candidatesCtrl', ['$scope', 'firebaseService', function($scope, 
     }, function(error) {
         console.log(error);
     });
+
+    
 
 }]);
